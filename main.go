181e413c6f68017839a34ecc2da82a0a519caa48
bank <a5820071@gmail.com>
2024-07-15 03:30:13 +0700

    edit api

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World! (edited)")
+		return c.SendString("Hello, World! (edited 2)")
 	})
 
 	app.Get("/test", func(c *fiber.Ctx) error {
